13cf5ef04a29ea3419f9c16dff4862b009ee49fa
Noelke10 <Noelke10@gmail.com>
2022-07-25 22:10:02 -0500

    added style to board/square added helper function

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Square from "./Square";
 
+const style = {
+  border: "4px solid darkblack",
+  borderRadius: "10px",
+  width: "400px",
+  height: "400px",
+  margin: "0 auto",
+  display: "grid",
+  gridTemplate: "repeat(3, 1fr) / repeat(3, 1fr)",
+};
+
 const Board = ({ squares, onClick }) => (
-  <div>
+  <div style={style}>
     <Square value="1" onClick={() => onClick("dummy Value")} />
     <Square value="2" onClick={() => onClick("dummy Value")} />
     <Square value="3" onClick={() => onClick("dummy Value")} />
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+const style = {
+  background: "lightGray",
+  border: "2px solid solid Gray",
+  fontSize: "30px",
+  fontWeight: "800",
+  cursor: "pointer",
+  outline: "none",
+};
+
 const Square = ({ value, onClick }) => (
-  <button onClick={onClick}>{value}</button>
+  <button style={style} onClick={onClick}>
+    {value}
+  </button>
 );
 
 export default Square;
